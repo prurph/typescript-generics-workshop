adc39971cc40b610150d35eb986fff32b53b4187
Prescott Murphy <prurph@gmail.com>
2024-09-04 17:31:43 -0400

    Complete Exercise 24

--- src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
+++ src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
@@ -1,7 +1,18 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function runGenerator(generator: unknown) {
+// Function overload solution
+function runGeneratorOverload<T>(generator: () => T): T;
+function runGeneratorOverload<T>(generator: { run: () => T }): T;
+function runGeneratorOverload<T>(generator: (() => T) | { run: () => T }) {
+  if (typeof generator === "function") {
+    return generator();
+  }
+  return generator.run();
+}
+// Direct union type solution: there's no need for the function overload here,
+// because the return type is the same for all types of parameters.
+function runGeneratorUnion<T>(generator: (() => T) | { run: () => T }) {
   if (typeof generator === "function") {
     return generator();
   }
@@ -9,19 +20,27 @@ function runGenerator(generator: unknown) {
 }
 
 it("Should accept an object where the generator is a function", () => {
-  const result = runGenerator({
+  const result = runGeneratorOverload({
+    run: () => "hello",
+  });
+  const result2 = runGeneratorUnion({
     run: () => "hello",
   });
 
   expect(result).toBe("hello");
+  expect(result2).toBe("hello");
 
   type test1 = Expect<Equal<typeof result, string>>;
+  type test2 = Expect<Equal<typeof result2, string>>;
 });
 
 it("Should accept an object where the generator is a function", () => {
-  const result = runGenerator(() => "hello");
+  const result = runGeneratorOverload(() => "hello");
+  const result2 = runGeneratorUnion(() => "hello");
 
   expect(result).toBe("hello");
+  expect(result2).toBe("hello");
 
   type test1 = Expect<Equal<typeof result, string>>;
+  type test2 = Expect<Equal<typeof result2, string>>;
 });
